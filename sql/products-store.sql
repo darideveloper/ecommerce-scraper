6b59ccacb435b92731d5d2e37cee2d5a5c91a55e
daridev <darideveloper@gmail.com>
2023-09-21 12:36:47 -0600

    feat (sql): add api tables

--- sql/products-store.sql
+++ sql/products-store.sql
@@ -8,12 +8,40 @@ CREATE TABLE `Products` (
   `best_seller` boolean,
   `sales` integer,
   `link` text,
-  `id_store` integer
+  `id_store` integer,
+  `id_request` integer
 );
 
 CREATE TABLE `Stores` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `use_proxies` bool
+);
+
+CREATE TABLE `ApiKeys` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `api_key` varchar(255),
+  `is_active` bool
+);
+
+CREATE TABLE `Requests` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `status` integer,
+  `start_datetime` datetime,
+  `end_datetime` datetime,
+  `api_key` integer
+);
+
+CREATE TABLE `Status` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255)
 );
 
 ALTER TABLE `Products` ADD FOREIGN KEY (`id_store`) REFERENCES `Stores` (`id`);
+
+ALTER TABLE `Products` ADD FOREIGN KEY (`id_request`) REFERENCES `Requests` (`id`);
+
+ALTER TABLE `Requests` ADD FOREIGN KEY (`status`) REFERENCES `Status` (`id`);
+
+ALTER TABLE `Requests` ADD FOREIGN KEY (`api_key`) REFERENCES `ApiKeys` (`id`);
